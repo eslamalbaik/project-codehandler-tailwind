70a22977ae464e696ab3ae50fab9be5ccd017e81
eslamD7 <eslamahmad2000t@gmail.com>
2022-11-12 12:22:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// function towersHeight(height) {
+//   var count = 0
+//   var maxValue = Math.max(...height)
+//   for (let i = 0; i < height.length; i++) {
+//     if (maxValue == height[i]) {
+//       ++count
+//     }
+//   }
+//   return count
+// }
+// console.log(towersHeight([4, 4, 1, 3]));
+// const numbers = [1, 2, 3, 4, 5];
+// const [...y] = numbers;
+
+// console.log(y);
+// const user = { name: 'Lydia', age: 21 };
+// const admin = { admin: true, ...user };
+
+// console.log(admin);
